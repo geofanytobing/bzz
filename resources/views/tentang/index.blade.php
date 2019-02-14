d619c3d6fb97f57ef726e48208f43d2091fe7bc8
Andrea <Andrea>
2019-02-14 17:23:02 +0700

    Update

--- resources/views/tentang/index.blade.php
+++ resources/views/tentang/index.blade.php
@@ -14,20 +14,96 @@
                     <div class="container undefined">
                         <div class="general-child">
                             <div class="about">
-                                <h2 class="about__title" data-test-id="page-title-about">Tentang Ruangguru</h2>
+                                <h2 class="about__title" data-test-id="page-title-about">Tentang Capstone Indonesia</h2>
                                 <div class="about__body">
                                     <section>
-                                        <p>Ruangguru merupakan perusahaan teknologi terbesar dan terlengkap di Indonesia yang berfokus pada layanan berbasis pendidikan dan telah memiliki lebih dari 6 juta pengguna serta telah mengelola lebih dari 150.000 guru yang menawarkan jasa di lebih dari 100 bidang pelajaran. Perusahaan ini didirikan sejak tahun 2014 oleh Belva Devara dan Iman Usman, yang keduanya berhasil masuk dalam jajaran pengusaha sukses dibawah 30 tahun melalui Forbes 30 under 30 untuk teknologi konsumen di Asia.</p>
-                                        <p>Ruangguru berkomitmen untuk menjadi mitra bagi pemerintah daerah demi memberikan pendidikan berkualitas melalui Sistem Manajemen Belajar (LMS). Tahun lalu, Ruangguru berhasil bekerja sama dengan 32 (dari 34) pemerintah provinsi dan lebih dari 326 pemerintah kota dan kabupaten di Indonesia. Selain itu, Ruangguru juga menawarkan video belajar berlangganan, marketplace les privat, layanan bimbingan belajar on-demand, tryout ujian online, dan lain-lain.</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                                     </section>
                                     <section class="about__video-wrapper">
-                                        <div class="about__player">
-    <video width="400" controls class="about__video-wrapper">
-      <source src="{{asset('images/tayo.mp4')}}" type="video/mp4">
-      Your browser does not support HTML5 video.
-    </video>
-  </div>
+                                     	<div class="about__player">
+    										<video width="400" controls class="about__video-wrapper">
+      											<source src="{{asset('images/tayo.mp4')}}" type="video/mp4">
+      											Your browser does not support HTML5 video.
+    										</video>
+ 			 							</div>
                                     </section>
+
+                                    <section class="about__section about__section--layout">
+                                    	<div>
+                                    		<h3 class="about__section-title kata">Capstone Untuk Pendidikan Indonesia</h3>
+                                    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    	</div>
+                                    	<div>
+                                    		<a class="link about__btn about__btn--orange about__btn--why" href="/help/student">Mengapa Belajar di Ruangguru?</a><a class="link about__btn about__btn--blue" href="/help/teacher">Mengapa Mengajar di Ruangguru?</a>
+                                    	</div>
+                                    </section>
+
+                                    <section>
+                                    <h3 class="about__section-title">Startup Digital di bidang pendidikan dan Teknologi terbaik di Indonesia</h3>
+                                    	<span>Sebagai salah satu startup teknologi dan pendidikan terbesar di Indonesia, Ruangguru telah dinobatkan menjadi penerima beragam penghargaan.</span>
+                                    	<div class="about__awards">
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/google-launchpad.png?auto=enhance%2Cformat%2Ccompress&amp;" alt="google launchpad"/><span>Google Launchpad Accelerator 2016 - Awardee</span>
+                                    		</div>
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/unicef.png?auto=enhance%2Cformat%2Ccompress&amp;" alt="unicef"/>
+                                    			<span>UNICEF Youth Innovation Forum 2015 - Innovation to Watch</span>
+                                    		</div>
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/bubu-awards-logo.png?auto=enhance%2Cformat%2Ccompress&amp;" alt="bubu awards"/>
+                                    			<span>Bubu Awards 2016 - Indonesia&#x27;s Best Education Web</span>
+                                    		</div>
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/solve-logo.png?auto=enhance%2Cformat%2Ccompress&amp;" alt="solve MIT award"/>
+                                    			<span>MIT Solve- Solver of MIT</span>
+                                    		</div>
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/atlassian-logo.png?auto=enhance%2Cformat%2Ccompress&amp;" alt="" class="about__atlassian"/>
+                                    			<span>the Atlassian Foundation - Atlassian Prize</span>
+                                    		</div>
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/australian-gov.png?auto=enhance%2Cformat%2Ccompress&amp;" alt=""/>
+                                    			<span>Departemen of Foreign Affairs</span>
+                                    			<span>and Trade - DFAT Indo -</span>
+                                    			<span>Pacific Development Prize</span>
+                                    		</div>
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/kinses-logo.png?auto=enhance%2Cformat%2Ccompress&amp;" alt="unicef" class="about__kinses"/>
+                                    			<span>
+                                    			Kaizen, INSEAD &amp; NYU Stern Education Symposium (KINSES 2016) - Honorable Mention</span>
+                                    		</div>
+                                    		<div class="about__award">
+                                    			<img src="https://ruangguru.imgix.net/png/ricebowl-awards.png?auto=enhance%2Cformat%2Ccompress&amp;" alt=""/>
+                                    			<span>Rice Bowl Startup Awards</span>
+                                    			<span>2016 - Best Social</span>
+                                    			<span>Entrepreneur for Tech</span>
+                                    			<span>Startup</span>
+                                    		</div>
+                                    	</div>
+                                    </section>
+
                                    </div>
                                </div>
                            </div>
